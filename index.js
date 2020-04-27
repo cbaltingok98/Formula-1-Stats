57303dca57e296d34f9947cd79d82657282e1731
Celik Berk Altingok <celikberkaltingok@Celiks-MacBook-Pro.local>
2020-04-26 21:20:47 -0700

    updated

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(bodyParser.json());
 
 const driverStat = require("./json/currentLeaderboard.json");
 const archive = require("./json/raceHistory.json");
+let foundRace = {};
 
 app.get("/", function(req, res){
     res.render("index", {driverStat : driverStat});
@@ -22,6 +23,19 @@ app.get("/archive", function(req, res) {
     res.render("archive", {archive : archive});
 });
 
+app.get("/archive/:id/show", function(req, res){
+    let raceID = req.params.id;
+    archive["2020"].map(race => {
+        if(race["race_id"] == raceID){
+            foundRace = race;
+        }
+    })
+    res.redirect("/showRaceStat");
+});
+
+app.get("/showRaceStat", function(req, res){
+    res.render("raceStats", {foundRace : foundRace});
+});
 
 app.listen(3000, '127.0.0.1', function () {
     console.log("Formula 1 Started");
