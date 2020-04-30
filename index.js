f6462849c8fc8065e26133a1888903894fea85eb
Celik Berk Altingok <celikberkaltingok@Celiks-MacBook-Pro.local>
2020-04-30 12:42:15 -0700

    2020 Driver Standings Updated

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 let driverStat = [
-    { "position": 1, "driver": "StarfoxF1", "team": "RACING POINT", "points": 341 },
-    { "position": 2, "driver": "FatedRacer14130", "team": "RACING POINT", "points": 312 },
-    { "position": 3, "driver": "GM ManyEx", "team": "FERRARI", "points": 215 },
-    { "position": 4, "driver": "Saloo911D", "team": "RENAULT", "points": 150 },
-    { "position": 5, "driver": "KillaRamo3", "team": "FERRARI", "points": 147 },
-    { "position": 6, "driver": "Yigitalp2631", "team": "MERCEDES", "points": 132 },
-    { "position": 7, "driver": "STPPH57", "team": "RENAULT", "points": 90 },
-    { "position": 8, "driver": "HoneyBadger6725", "team": "REDBULL", "points": 88 },
-    { "position": 9, "driver": "SilaHeNdaZ", "team": "MERCEDES", "points": 32 },
-    { "position": 10, "driver": "OpsAngel98", "team": "REDBULL", "points": 27 }
+    { "driver": "StarfoxF1", "team": "RACING POINT", "points": 356 },
+    { "driver": "FatedRacer14130", "team": "RACING POINT", "points": 330 },
+    { "driver": "GM ManyEx", "team": "FERRARI", "points": 241 },
+    { "driver": "Saloo911D", "team": "RENAULT", "points": 150 },
+    { "driver": "KillaRamo3", "team": "FERRARI", "points": 147 },
+    { "driver": "Yigitalp2631", "team": "MERCEDES", "points": 144 },
+    { "driver": "STPPH57", "team": "RENAULT", "points": 90 },
+    { "driver": "HoneyBadger6725", "team": "REDBULL", "points": 94 },
+    { "driver": "SilaHeNdaZ", "team": "MERCEDES", "points": 40 },
+    { "driver": "OpsAngel98", "team": "REDBULL", "points": 37 }
 ];
 
 driverStat.sort(function(a, b) {
@@ -21,12 +21,13 @@ driverStat.sort(function(a, b) {
         table = table[3].childNodes;
         table = table[3];
         let counter = 0;
+        let positionCounter = 1;
         driverStat.map(driver  => {
             let row = document.createElement('tr');
             table.appendChild(row);
             let newRow = table.childNodes[counter];
             let dPosition = document.createElement('td');
-            dPosition.textContent = driver.position;
+            dPosition.textContent = positionCounter;
             newRow.appendChild(dPosition);
             let dName = document.createElement('td');
             dName.textContent = driver.driver;
@@ -38,4 +39,5 @@ driverStat.sort(function(a, b) {
             dPoints.textContent = driver.points;
             newRow.appendChild(dPoints);
             counter++;
+            positionCounter++;
         });
